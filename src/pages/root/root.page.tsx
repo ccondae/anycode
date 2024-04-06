44aa0d1f803f262dd77dae2dcb24d53d6a85574b
yongveloper <aaa9015@naver.com>
2024-04-07 00:14:26 +0900

    style: question-list 컴포넌트 마크업&스타일링

--- src/pages/root/root.page.tsx
+++ src/pages/root/root.page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "~/widgets/header";
+import { QuestionList } from "~/widgets/question-list";
 
 export const RootPage = () => {
   return (
     <>
       <Header />
+      {/* Todo: 사이드바 작업한 거랑 레이아웃 합쳐야함 */}
+      <QuestionList />
     </>
   );
 };
