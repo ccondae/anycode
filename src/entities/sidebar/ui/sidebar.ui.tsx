95e392aea9a3ebfd291cf6d9d3429ee86073ca76
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-04-06 23:25:46 +0900

    ✨ Box-shadow 추가

--- src/entities/sidebar/ui/sidebar.ui.tsx
+++ src/entities/sidebar/ui/sidebar.ui.tsx
@@ -9,13 +9,13 @@ const Container = styled.div`
   min-width: 168px;
   height: 429px;
   border-radius: 30px;
-  border: 1px solid white;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   gap: 30px;
   padding: 40px 30px;
+  box-shadow: 0px 4px 50px 0px rgba(255, 255, 255, 0.1);
 `;
 
 const Title = styled.div`
