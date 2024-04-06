d9f3bccfa45197fbc4a3447cc368e8e0cba55ed2
Yong <aaa9015@naver.com>
2024-04-06 15:39:17 +0900

    styled-components 토큰 추가 (#3)
    * style:  theme type 정의
    타입 추론을 위한 타입 정의
    
    * style: 색상 및 폰트 사이즈 토큰 추가
    
    * style: ThemeProvider 적용
    
    * refactor: 디자인 토큰 관련 계층 이동
    shared 계층에서도 common-ui 정의를 위해 사용되어야하는 의존성인 이유로 이동함

--- src/app/provider/global-styled-provider.tsx
+++ src/app/provider/global-styled-provider.tsx
@@ -1,14 +1,25 @@
-import { createGlobalStyle } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+import { vars } from "~/shared/common-ui/theme";
+
 export const GlobalStyle = createGlobalStyle`
-    ${reset}
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.black};
+    color: ${(props) => props.theme.colors.white};
+  }
 `;
 
 export const GlobalStyleProvider = () => {
   return (
-    <>
+    <ThemeProvider theme={vars}>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 };
---
+++ src/shared/common-ui/theme/styled.d.ts
@@ -0,0 +1,31 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+        tabBar: string,
+        main:string,
+        secondary:string,
+        action:string,
+        white: string,
+        black:string,
+        view: string,
+        answer: string,
+        like: string,
+      },
+      fontSize: {
+        headline1: string,
+        headline2: string,
+        headline3:string,
+        headline4: string,
+        headline5: string,
+        headline6: string,
+        subtitle1: string,
+        subtitle2: string,
+        body1:string,
+        body2: string,
+        button:string,
+        caption: string,
+      },
+  }
+}
