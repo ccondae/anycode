ea58cf355bbd44c9ea8d4bd2d24bbec04e18d211
yongveloper <aaa9015@naver.com>
2024-04-07 18:22:54 +0900

    style: Main 가운데 정렬

--- src/pages/root/root.page.tsx
+++ src/pages/root/root.page.tsx
@@ -12,6 +12,7 @@ import { Banner } from "~/shared/banner";
 const Main = styled.main`
   width: 100%;
   max-width: 1440px;
+  margin: auto;
   display: flex;
   justify-content: center;
   align-items: flex-start;
