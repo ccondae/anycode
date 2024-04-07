b6c75c6ea12c77c7d94354ca1aa073b772d37ce2
yongveloper <aaa9015@naver.com>
2024-04-07 19:32:56 +0900

    chore: 주석 추가

--- src/entities/question-list/api/use-question-list.query.ts
+++ src/entities/question-list/api/use-question-list.query.ts
@@ -3,6 +3,8 @@ import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { QuestionListType } from "../model/question-list.type";
 import { QUESTION_LIST_ENDPOINT, QUESTION_LIST_KEY } from "./question-list.key";
 
+// Todo: 현재는 "popular"로 고정되어있지만, 추후에는 인자로 받아서 사용할 수 있도록 변경해야합니다.
+// "전체","답변된 질문","답변되지 않은 질문" 등등..
 export const useQuestionListQuery = (page: number): UseQueryResult<QuestionListType> => {
   const questionList = useQuery<QuestionListType>({
     queryKey: QUESTION_LIST_KEY.pagination({ size: 10, page }),
@@ -14,19 +16,3 @@ export const useQuestionListQuery = (page: number): UseQueryResult<QuestionListT
   });
   return questionList;
 };
-
-/*
- 페이지네이션 쿼리가 반환해야할 내용
-
-1. 다음으로 가기 함수
-2. 이전으로 가기 함수
-3. 특정페이지로 가기 함수
-4. 현재 페이지에 대한 상태
-5. useQuery에 대한 결과물
-
-1 ~ 4는 매번 페이지네이션 필요할때마다 하드코딩하면 공수가 매우커지니까 한번에 처리해주면    좋을것같읍니다
-
-주의해야할점
-이전으로가기는 0미만으로 떨어지면안됩니다
-다음으로가기는 다음페이지가 없으면 가면안됩니다
-*/
