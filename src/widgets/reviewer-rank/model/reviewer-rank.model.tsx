b63c9a3d8b43ab7136929e6639b09f97a7e75fe0
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-04-08 01:27:03 +0900

    🐛 Github 링크 수정

--- src/widgets/reviewer-rank/model/reviewer-rank.model.tsx
+++ src/widgets/reviewer-rank/model/reviewer-rank.model.tsx
@@ -1,10 +1,10 @@
 export const mockData = [
-  { id: 0, username: "하늘", url: "https://github.com/orgs/ccondae/people/Leedo02" },
-  { id: 1, username: "로키", url: "https://github.com/orgs/ccondae/people/rookedsysc" },
+  { id: 0, username: "하늘", url: "https://github.com/Leedo02" },
+  { id: 1, username: "로키", url: "https://github.com/rookedsysc" },
   { id: 2, username: "르미", url: "/ruemi" },
   { id: 3, username: "미오", url: "https://github.com/Jaeeun98" },
-  { id: 4, username: "삽사리", url: "https://github.com/orgs/ccondae/people/Sang-minKIM" },
-  { id: 5, username: "시온", url: "https://github.com/orgs/ccondae/people/XionWCFM" },
-  { id: 6, username: "운터", url: "https://github.com/orgs/ccondae/people/anveloper" },
+  { id: 4, username: "삽사리", url: "https://github.com/Sang-minKIM" },
+  { id: 5, username: "시온", url: "https://github.com/XionWCFM" },
+  { id: 6, username: "운터", url: "https://github.com/anveloper" },
   { id: 7, username: "준", url: "https://github.com/Yongveloper" },
 ];
