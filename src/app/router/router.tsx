a6b06b565a7519d41644691db39164d47c32a7a1
devgiljong <calf-love@naver.com>
2024-04-07 20:43:16 +0900

    :sparkles: Progress 컴포넌트 젝

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -4,9 +4,6 @@ import { QuestionPage } from "~/pages/question";
 import QuestionWritePage from "~/pages/question/question-write.page";
 import { RootPage } from "~/pages/root";
 
-import { CommentForm, CommentUploadContextProvder } from "~/features/comment";
-
-import { vars } from "~/shared/common-ui/theme";
 import { ROUTE } from "~/shared/route";
 
 export const router = createBrowserRouter([
@@ -21,31 +18,5 @@ export const router = createBrowserRouter([
   {
     path: ROUTE.questionWrite,
     element: <QuestionWritePage />,
-  },
-  {
-    path: "/progress",
-    element: (
-      <CommentUploadContextProvder>
-        <div
-          style={{
-            width: "100vw",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingTop: "60px",
-          }}
-        >
-          <div
-            style={{
-              width: "768px",
-              backgroundColor: vars.colors.like,
-              paddingInline: "20px",
-            }}
-          >
-            <CommentForm questionId="1" />
-          </div>
-        </div>
-      </CommentUploadContextProvder>
-    ),
   },
 ]);
