5b731bef007bd9137bb07cea32d32f317b1705c5
devgiljong <calf-love@naver.com>
2024-04-08 00:42:10 +0900

    :sparkles: 로그인 버튼 르미납치 기능 추가

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export * from "./use-localstorage";
 export * from "./use-editor";
 export * from "./context-generator";
 export * from "./use-page-nation";
+export * from './use-reumi'
--- src/widgets/question-list/ui/question-list.ui.tsx
+++ src/widgets/question-list/ui/question-list.ui.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
 import { Question } from "~/entities/question";
 import { useQuestionListQuery } from "~/entities/question-list/api/use-question-list.query";
 
 import { PageNation } from "~/shared/common-ui/page-nation";
-import { useToast } from "~/shared/common-ui/toast";
-import { ROUTE } from "~/shared/route";
-import { delay } from "~/shared/utils";
+import { useReumi } from "~/shared/hooks";
 
 const Container = styled.div`
   max-width: 768px;
@@ -32,21 +29,10 @@ const QuestionListContainer = styled.div`
 export const QuestionList = () => {
   const [searchParams] = useSearchParams();
   const currentPage = Number(searchParams?.get("page")) || 1;
-  const navigate = useNavigate();
-  const [isClicked, setIsClicked] = useState(false);
-  const { toast, toasts } = useToast();
+  const { goToReumi } = useReumi();
   // Todo: 현재는 "popular"로 고정되어있지만, 인자로 받아서 사용할 수 있도록 변경해야합니다.
   // "전체","답변된 질문","답변되지 않은 질문" 등등..
   const { data: questions, isPending, isError } = useQuestionListQuery(currentPage - 1);
-  console.log(toasts);
-  const questionOnClick = async () => {
-    if (isClicked) return;
-    setIsClicked(true);
-    toast({ title: "대신귀 여운르 미를 보여드 리갰습 니다" });
-    await delay(3000);
-    setIsClicked(false);
-    navigate(ROUTE.reumi);
-  };
 
   if (isPending) {
     return <div>Loading...</div>;
@@ -68,7 +54,7 @@ export const QuestionList = () => {
             categories={categories}
             commentCount={commentCount}
             createdAt={createdAt}
-            onClick={questionOnClick}
+            onClick={goToReumi}
           />
         ))}
       </QuestionListContainer>
