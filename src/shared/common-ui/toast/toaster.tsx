caa931df44c42582da6df1dff98022e4f65e325b
devgiljong <calf-love@naver.com>
2024-04-07 19:24:09 +0900

    :sparkles: Toast 생김새 개선

--- src/shared/common-ui/toast/toaster.tsx
+++ src/shared/common-ui/toast/toaster.tsx
@@ -4,13 +4,14 @@ import { useToast } from "./use-toast";
 export const Toaster = () => {
   const { toasts } = useToast();
   return (
-    <ToastProvider duration={1000}>
+    <ToastProvider duration={5000}>
       {toasts.map(({ id, title, action, ...props }) => (
         <Toast key={id} {...props}>
           <div className=" grid ">{title}</div>
           {action}
         </Toast>
       ))}
+
       <ToastViewport />
     </ToastProvider>
   );
