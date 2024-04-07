799df24d4b76f271fc9e269a59a7c07a5101a5d0
devgiljong <calf-love@naver.com>
2024-04-07 17:36:11 +0900

    :construction: 테스트 설명 추가

--- __tests__/fetchmock.test.ts
+++ __tests__/fetchmock.test.ts
@@ -1,5 +1,5 @@
-describe("", () => {
-  it("", async () => {
+describe("fetch mock의 동작을 확인합니다.", () => {
+  it("mockResponse를 통하여 응답을 모킹합니다.", async () => {
     //@ts-ignore
     fetch.mockResponse(JSON.stringify({ hello: "world" }));
     const response = await fetch("hi");
