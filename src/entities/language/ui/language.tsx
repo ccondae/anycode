13f5473109e04c5fdba52e5468e3d515bfaa8c85
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-04-08 17:17:16 +0900

    ✨ 선택된 언어 동작 스타일 적용

--- src/entities/language/ui/language.tsx
+++ src/entities/language/ui/language.tsx
@@ -1,18 +1,34 @@
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
+
+import { languageState } from "~/widgets/language-rank/model/language-rank.atom";
 
 interface LanguageProps {
   language: string;
+  onLanguageClick: () => void;
+  id: number;
 }
 
-const Item = styled.div`
+const Item = styled.button`
   width: 100%;
   height: 21px;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 16px;
+  background-color: transparent;
+  border: none;
+  color: ${({ theme }) => theme.colors.secondary};
+  &.active {
+    color: ${({ theme }) => theme.colors.white};
+  }
 `;
 
-export const Language = ({ language }: LanguageProps) => {
-  return <Item>{language}</Item>;
+export const Language = ({ language, onLanguageClick, id }: LanguageProps) => {
+  const activeLanguageId = useRecoilValue(languageState);
+  return (
+    <Item onClick={onLanguageClick} className={activeLanguageId === id ? "active" : ""}>
+      {language}
+    </Item>
+  );
 };
