99d3d114a14fcef5cd1c9084d6b246d4a77a142b
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-04-08 17:15:51 +0900

    ✨ 언어별 필터링을 위해 쿼리함수 수정

--- src/entities/question-list/api/question-list.key.ts
+++ src/entities/question-list/api/question-list.key.ts
@@ -2,13 +2,13 @@ import { ENV } from "~/shared/environment";
 
 export const QUESTION_LIST_ENDPOINT = {
   // default: `${ENV.baseUrl}/api/question/page/popular`,
-  pagination: (body: { category: string; size: number; page: number }) =>
-    `${ENV.baseUrl}/api/question/${body.category}?size=${body.size}&page=${body.page}`,
+  pagination: (body: { category: string; size: number; page: number; language: number }) =>
+    `${ENV.baseUrl}/api/question/${body.category}?categories=${body.language || ""}&size=${body.size}&page=${body.page}`,
 };
 
 export const QUESTION_LIST_KEY = {
   default: ["question-list"],
-  pagination: (body: { category: string; size: number; page: number }) => {
-    return [...QUESTION_LIST_KEY.default, body.category, body.size, body.page];
+  pagination: (body: { category: string; size: number; page: number; language: number }) => {
+    return [...QUESTION_LIST_KEY.default, body.category, body.size, body.page, body.language];
   },
 };
--- src/entities/question-list/api/use-question-list.query.ts
+++ src/entities/question-list/api/use-question-list.query.ts
@@ -3,11 +3,15 @@ import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { QuestionListType } from "../model/question-list.type";
 import { QUESTION_LIST_ENDPOINT, QUESTION_LIST_KEY } from "./question-list.key";
 
-export const useQuestionListQuery = (category: string, page: number): UseQueryResult<QuestionListType> => {
+export const useQuestionListQuery = (
+  category: string,
+  page: number,
+  language: number
+): UseQueryResult<QuestionListType> => {
   const questionList = useQuery<QuestionListType>({
-    queryKey: QUESTION_LIST_KEY.pagination({ category, size: 10, page }),
+    queryKey: QUESTION_LIST_KEY.pagination({ category, size: 10, page, language }),
     queryFn: async () => {
-      const response = await fetch(QUESTION_LIST_ENDPOINT.pagination({ category, size: 10, page }));
+      const response = await fetch(QUESTION_LIST_ENDPOINT.pagination({ category, size: 10, page, language }));
       const json = await response.json();
       return json;
     },
