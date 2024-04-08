3db21a5c0d0db3af507645ba4bba4bd70f698580
yongveloper <aaa9015@naver.com>
2024-04-08 20:14:49 +0900

    chore: FSD는 index에서 가져와야 하는 규칙 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { GlobalSuspenseBoundary, ProviderList } from "~/app/provider";
-import { router } from "~/app/router/router";
+import { router } from "~/app/router";
 import "~/app/style/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
