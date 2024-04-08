82fb0401ae5890dfa66d6bd7a75dcc7fb1d48cff
yongveloper <aaa9015@naver.com>
2024-04-08 20:52:10 +0900

    chore: 불필요한 라우트 제거

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { QuestionPage } from "~/pages/question";
 import { QuestionDetailPage } from "~/pages/question-detail-page/question-detail-page";
 import QuestionWritePage from "~/pages/question/question-write.page";
 import { ReumiPage } from "~/pages/reumi";
@@ -20,7 +19,6 @@ export const router = createBrowserRouter([
         path: ROUTE.questionDetail,
         element: <QuestionDetailPage />,
       },
-      { path: ROUTE.question, element: <QuestionPage /> },
       { path: ROUTE.questionWrite, element: <QuestionWritePage /> },
       { path: ROUTE.reumi, element: <ReumiPage /> },
     ],
