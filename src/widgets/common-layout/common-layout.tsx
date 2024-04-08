879cda59b1c8a53e04c85a9a8489c8138d4c0555
yongveloper <aaa9015@naver.com>
2024-04-08 20:16:18 +0900

    feat: 기본 Layout 컴포넌트 생성

---
+++ src/widgets/common-layout/common-layout.tsx
@@ -0,0 +1,47 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+import { Banner } from "~/shared/banner";
+
+import { Header } from "../header";
+import { LanguageRank } from "../language-rank";
+import { QuestionGuide } from "../question-guide";
+import { ReviewerRank } from "../reviewer-rank";
+import { Top } from "../top-filter";
+
+const Main = styled.main`
+  width: 100%;
+  max-width: 1440px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 10px;
+  gap: 10px;
+`;
+
+const LeftSideBars = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const CommonLayout = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Top />
+      <Main>
+        <LeftSideBars>
+          <ReviewerRank />
+          <LanguageRank />
+        </LeftSideBars>
+        <Outlet />
+        <QuestionGuide />
+      </Main>
+    </>
+  );
+};
