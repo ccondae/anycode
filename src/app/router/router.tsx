82817376029809356670a6eac67badda6c078fb0
yongveloper <aaa9015@naver.com>
2024-04-08 20:17:04 +0900

    feat: router CommonLayout 컴포넌트 적용

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -5,23 +5,19 @@ import QuestionWritePage from "~/pages/question/question-write.page";
 import { ReumiPage } from "~/pages/reumi";
 import { RootPage } from "~/pages/root";
 
+import { CommonLayout } from "~/widgets/common-layout";
+
 import { ROUTE } from "~/shared/route";
 
 export const router = createBrowserRouter([
   {
-    path: ROUTE.root,
-    element: <RootPage />,
-  },
-  {
-    path: ROUTE.question,
-    element: <QuestionPage />,
-  },
-  {
-    path: ROUTE.questionWrite,
-    element: <QuestionWritePage />,
-  },
-  {
-    path: ROUTE.reumi,
-    element: <ReumiPage />,
+    path: "/",
+    element: <CommonLayout />,
+    children: [
+      { path: ROUTE.root, element: <RootPage /> },
+      { path: ROUTE.question, element: <QuestionPage /> },
+      { path: ROUTE.questionWrite, element: <QuestionWritePage /> },
+      { path: ROUTE.reumi, element: <ReumiPage /> },
+    ],
   },
 ]);
